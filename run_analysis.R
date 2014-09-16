d1dcdf0fd55b06f5acc814a506d50a65c552ecd7
piobandu <redroy44@gmail.com>
2014-09-16 23:34:21 +0200

    modified the script

--- run_analysis.R
+++ run_analysis.R
@@ -2,12 +2,17 @@ library(dplyr)
 
 url <- "http://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 
-download.file(url, "dataset.zip")
-unzip("dataset.zip")
+#download.file(url, "dataset.zip")
+#unzip("dataset.zip")
 archiveName <- "./UCI HAR Dataset"
 
-trainFiles <- list.files(archiveName, recursive = TRUE, pattern = "*train.txt")
-testFiles <- list.files(archiveName, recursive = TRUE, pattern = "*test.txt")
+trainFiles <- list.files(file.path(archiveName, "train"), recursive = FALSE, pattern = "*.txt")
+#testFiles <- list.files(file.path(archiveName, "test"), recursive = FALSE, pattern = "*.txt")
+
+# prepare output dir
+if (!file.exists("merged")) {
+  dir.create("merged")
+} 
 
 #select mean and std features
 featFile <- file.path(archiveName, "features.txt")
@@ -15,7 +20,24 @@ feat <- read.table(featFile)
 
 l <- grep("mean|std", feat[,2], ignore.case = TRUE)
 
-# now, merge the datasets
+subject <- file.path(archiveName, "train", "subject_train.txt")
+X <- file.path(archiveName, "train", "X_train.txt")
+y <- file.path(archiveName, "train", "y_train.txt")
 
+train <- read.table(subject)
+test <- read.table(gsub("train", "test", subject))
+subject_merged <- rbind(train, test)
 
+train <- read.table(y)
+test <- read.table(gsub("train", "test", y))
+y_merged <- rbind(train, test)
+
+train <- read.table(X)
+test <- read.table(gsub("train", "test", X))
+merged <- rbind(train, test)
+names(merged) <- feat$V2
+merged <- merged[, l]
+merged <- cbind(y_merged, merged)
+merged <- cbind(subject_merged, merged)
+write.table(merged, file = "merged/X_merge.txt", row.name = FALSE)
 
